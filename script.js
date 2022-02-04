5e35405351ad09a0cf084813f38408b500efdf8d
frenchmandarinka <95296641+frenchmandarinka@users.noreply.github.com>
2022-02-04 03:56:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// alert('Пуньк');
+
+// function sayHello() {
+//   alert('Привет, Андрей');
+// }
+
+// sayHello();
+
+function sayHello(name) {
+  alert('Привет, ' + name); // name — параметр
+}
+
+sayHello('Андрей');
+
+// function sayHello(name) {
+//   return 'Привет, ' + name;
+// }
+
+// let greeting = sayHello('Венера');
+
+// console.log(greeting); // "Привет, Венера" — вывели в консоль
+// alert(greeting); // "Привет, Венера" — показали в модальном окне
